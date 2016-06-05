00ebcb7414e0947db6c146d80d324abe138fd1ff
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-05 22:16:26 +0200

    Recognize osm bounds, ways, nodes

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
 
 public class Main {
+    public static int minLat;
+    public static int minLon;
+    public static int maxLat;
+    public static int maxLon;
+
     public static void main(String[] args) {
         try {
             CharStream input = new ANTLRFileStream(args[0]);
@@ -11,30 +16,63 @@ public class Main {
             OSMParser parser = new OSMParser(tokens);
             OSMParser.document_return root = parser.document();
             CommonTree root_tree = (CommonTree) root.tree;
-            printTree(root_tree, 0);
-            // System.out.println("tree=" + (root_tree).toStringTree());
-            // CommonTreeNodeStream nodes = new CommonTreeNodeStream((Tree)root.tree);
-            // OSMTreeParser walker = new OSMTreeParser(nodes);
-            // walker.document();
+            walkTree(root_tree, 0);
         } catch (Throwable t) {
             System.out.println("exception: " + t);
             t.printStackTrace();
         }
     }
 
-    public static void printTree(CommonTree t, int indent) {
+    public static void walkTree(CommonTree t, int indent) {
         if (t != null) {
             StringBuffer sb = new StringBuffer(indent);
 
-            if (t.getParent() == null) {
-                System.out.println(sb.toString() + t.getText().toString());
-            }
-            for (int i = 0; i < indent; i++)
-                sb = sb.append("   ");
             for (int i = 0; i < t.getChildCount(); i++) {
-                System.out.println(sb.toString() + t.getChild(i).toString());
-                printTree((CommonTree) t.getChild(i), indent + 1);
+                Tree child = t.getChild(i);
+                if (child.getChildCount() == 0) continue;
+
+                if (!isAttribute(child)) {
+                    if (isBounds(child))
+                        handleBounds(child);
+                    else if (isNode(child))
+                        handleNode(child);
+                    else if (isWay(child))
+                        handleWay(child);
+                    else
+                        System.out.println(child.toString());
+                }
             }
         }
+    }
+
+    public static boolean isAttribute(Tree t) {
+      return t.getText().toString().equals("ATTRIBUTE");
+    }
+
+    public static boolean isWay(Tree t) {
+      return t.getChild(0).getText().toString().equals("way");
+    }
+
+    public static boolean isNode(Tree t) {
+      return t.getChild(0).getText().toString().equals("node");
+    }
+
+    public static boolean isBounds(Tree t) {
+      return t.getChild(0).getText().toString().equals("bounds");
+    }
+
+    public static void handleWay(Tree t) {
+      System.out.println("\nI'm handling a way:");
+      System.out.println(t.toStringTree());
+    }
+
+    public static void handleNode(Tree t) {
+      System.out.println("\nI'm handling a node:");
+      System.out.println(t.toStringTree());
+    }
+
+    public static void handleBounds(Tree t) {
+      System.out.println("\nI'm handling bounds:");
+      System.out.println(t.toStringTree());
     }
 }
