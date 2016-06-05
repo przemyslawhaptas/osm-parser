091f12e3e65f9f8efcfc64156d09cd5dcf98bdff
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2016-06-05 19:52:56 +0200

    Dump TreeParser, read data straight from Parsers AST

--- Main.java
+++ Main.java
@@ -3,21 +3,38 @@ import org.antlr.runtime.tree.*;
 
 public class Main {
     public static void main(String[] args) {
-     try {
-         CharStream input = new ANTLRFileStream(args[0]);
-         OSMLexer lex = new OSMLexer(input);
+        try {
+            CharStream input = new ANTLRFileStream(args[0]);
+            OSMLexer lex = new OSMLexer(input);
 
-         CommonTokenStream tokens = new CommonTokenStream(lex);
-         OSMParser parser = new OSMParser(tokens);
-         OSMParser.document_return root = parser.document();
-         System.out.println("tree="+((Tree)root.tree).toStringTree());
+            CommonTokenStream tokens = new CommonTokenStream(lex);
+            OSMParser parser = new OSMParser(tokens);
+            OSMParser.document_return root = parser.document();
+            CommonTree root_tree = (CommonTree) root.tree;
+            printTree(root_tree, 0);
+            // System.out.println("tree=" + (root_tree).toStringTree());
+            // CommonTreeNodeStream nodes = new CommonTreeNodeStream((Tree)root.tree);
+            // OSMTreeParser walker = new OSMTreeParser(nodes);
+            // walker.document();
+        } catch (Throwable t) {
+            System.out.println("exception: " + t);
+            t.printStackTrace();
+        }
+    }
 
-         CommonTreeNodeStream nodes = new CommonTreeNodeStream((Tree)root.tree);
-         OSMTreeParser walker = new OSMTreeParser(nodes);
-         walker.document();
-     } catch(Throwable t) {
-         System.out.println("exception: "+t);
-         t.printStackTrace();
-     }
+    public static void printTree(CommonTree t, int indent) {
+        if (t != null) {
+            StringBuffer sb = new StringBuffer(indent);
+
+            if (t.getParent() == null) {
+                System.out.println(sb.toString() + t.getText().toString());
+            }
+            for (int i = 0; i < indent; i++)
+                sb = sb.append("   ");
+            for (int i = 0; i < t.getChildCount(); i++) {
+                System.out.println(sb.toString() + t.getChild(i).toString());
+                printTree((CommonTree) t.getChild(i), indent + 1);
+            }
+        }
     }
 }
